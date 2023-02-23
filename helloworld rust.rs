72c5f66a7949b84dd40643c463a5d4b49d9f4eb9
anilyagiz <anill.yagiz@gmail.com>
2023-02-23 17:25:22 +0300

    Update helloworld rust.rs

--- helloworld rust.rs
+++ helloworld rust.rs
@@ -1,4 +1,22 @@
-fn main(){
-   println!("Hello World!");
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg};
+entrypoint!(process_instruction);
+pub fn process_instruction (
 
-}
+   _program_id:Pubkey,
+   _accounts: &[AccountInfo],
+   _instruction_data: &[u8],
+
+
+)
+  -> ProgramResults{
+   msg!("Hello World!");
+   Ok(())
+
+
+
+   }
